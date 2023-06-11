d39951da2e0e07c9e363a63b77305932f6af814d
Jesse Florig <jesseflorig@gmail.com>
2023-06-11 10:22:41 -0500

    added challenges and solves to gql

--- index.ts
+++ index.ts
@@ -7,8 +7,22 @@ const typeDefs = `#graphql
     name: String
   }
 
+  type Challenge {
+    id: Int
+    name: String
+    description: String
+    flag: String
+  }
+
+  type Solve {
+    team: Team
+    challenge: Challenge
+  }
+
   type Query {
     teams: [Team]
+    challenges: [Challenge]
+    solves: [Solve]
   }
 `;
 
@@ -18,9 +32,20 @@ const teams = [
   {id: 3, name: 'Team 3'},
 ]
 
+const challenges = [
+  {id: 1, name: 'Challenge 1', description: 'This is an easy one', flag: '123456'},
+  {id: 2, name: 'Challenge 2', description: 'Another one', flag: '987654'}
+]
+
+const solves = [
+  {team: teams[0], challenge: challenges[0]}
+]
+
 const resolvers = {
   Query: {
     teams: () => teams,
+    challenges: () => challenges,
+    solves: () => solves
   }
 }
 
